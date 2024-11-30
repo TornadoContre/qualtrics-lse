62945b44ac095590ee681479632da22b1ae62a4d
Ignacio Contreras <ignaciocontrerascaceres@gmail.com>
2024-11-30 12:01:35 +0000

    shocks

---
+++ survey-questionnaire/shocks.js
@@ -0,0 +1,16 @@
+Qualtrics.SurveyEngine.addOnload(function () {
+	/*Place your JavaScript here to run when the page loads*/
+
+});
+
+Qualtrics.SurveyEngine.addOnReady(function () {
+	/*Place your JavaScript here to run when the page is fully displayed*/
+	// Overflow
+	jQuery(".QuestionBody").css('overflow', 'visible');
+	jQuery(".BorderColor").css('overflow', 'visible');
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function () {
+	/*Place your JavaScript here to run when the page is unloaded*/
+
+});
